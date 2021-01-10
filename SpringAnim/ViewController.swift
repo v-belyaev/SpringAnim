c94e2acdfab903c080d5dc21fc38e179663d06cd
Vladimir Belyaev <jokerhate.edm@gmail.com>
2021-01-10 03:49:39 +0300

    Initial Commit

---
+++ SpringAnim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnim
+//
+//  Created by Владимир Беляев on 10.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
